d188bc54661b936c35a95c7933ed5b3e84c15f0b
Vlad H <vlad.hutupasu@yahoo.com>
2018-05-14 18:58:20 +0200

    Train & Test data now fixed

--- SVM_bitcoin.py
+++ SVM_bitcoin.py
@@ -12,30 +12,25 @@ import pickle
 
 def predict_movement(close_off_high, day_diff):
     if(model.predict([[close_off_high, day_diff]])) == 0:
-        # print('Down movement')
         return 'Down'
     else:
-        # print('Upwards movement')
         return 'Up'
 
-train_data = pd.read_csv('features2017test.csv')
-test_data = pd.read_csv('features2018.csv')
+train_data = pd.read_csv('2017_train_data.csv')
+test_data = pd.read_csv('2018_test_data.csv')
 
-# test_data = test_data[0:100]
 print('Length train data: '+str(len(train_data)))
 print('Length test data: '+str(len(test_data)))
 
-# Plot two ingredients
+# Plot two features
 sns.lmplot('Close Off High', 'Day Diff', data=train_data, hue='Movement',
            palette='Set1', fit_reg=False, scatter_kws={"s": 70})
 plt.show()
 
 
 # Specify inputs for the model
-# ingredients = recipes[['Flour', 'Milk', 'Sugar', 'Butter', 'Egg', 'Baking Powder', 'Vanilla', 'Salt']].as_matrix()
-imp_features = train_data[['Close Off High','Day Diff']].as_matrix()
+imp_features = train_data[['Close Off High', 'Day Diff']].as_matrix()
 movement_label = np.where(train_data['Movement']=='Up', 1, 0)
-
 
 # Feature names
 bitcoin_features = train_data.columns.values[1:].tolist()
@@ -46,51 +41,54 @@ bitcoin_features = train_data.columns.values[1:].tolist()
 model = svm.SVC(kernel='linear')
 model.fit(imp_features, movement_label)
 
-i=0
-accuracy=0
-
-while i < len(test_data):
-
-    result = predict_movement(test_data['Close Off High'][i], test_data['Day Diff'][i])
-    if(result == test_data['Movement'][i]):
-        accuracy=accuracy+1
-
-    i = i + 1
-
-final_accuracy = accuracy/len(test_data) *100
-print('ACCURACY IS:'+str(final_accuracy)+'%')
-
-
-
-
-
-
-# # Get the separating hyperplane
-# w = model.coef_[0]
-# a = -w[0] / w[1]
-# xx = np.linspace(-10000, 10000) #min - max feature value
-# yy = a * xx - (model.intercept_[0]) / w[1]
+# i=0
+# accuracy=0
 #
-# # Plot the parallels to the separating hyperplane that pass through the support vectors
-# b = model.support_vectors_[0]
-# yy_down = a * xx + (b[1] - a * b[0])
-# b = model.support_vectors_[-1]
-# yy_up = a * xx + (b[1] - a * b[0])
+# while i < len(test_data):
 #
-# # Plot the hyperplane
-# sns.lmplot('Close Off High','Day Diff', data=train_data, hue='Movement', palette='Set1', fit_reg=False, scatter_kws={"s": 70})
-# plt.plot(xx, yy, linewidth=2, color='black');
-# plt.show()
+#     result = predict_movement(test_data['Close Off High'][i], test_data['Day Diff'][i])
+#     if(result == test_data['Movement'][i]):
+#         print('Accuracy for: ' + str(i) + ' -> TRUE')
+#         accuracy=accuracy+1
+#     else:
+#         print('Accuracy for: ' + str(i) + ' -> FALSE')
 #
+#     i = i + 1
 #
-# # Look at the margins and support vectors
-# sns.lmplot('Close Off High', 'Day Diff', data=train_data, hue='Movement', palette='Set1', fit_reg=False, scatter_kws={"s": 70})
-# plt.plot(xx, yy, linewidth=2, color='black')
-# plt.plot(xx, yy_down, 'k--')
-# plt.plot(xx, yy_up, 'k--')
-# plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1],
-#             s=80, facecolors='none');
-# plt.show()
+# final_accuracy = accuracy/len(test_data) *100
+# print('ACCURACY IS:'+str(final_accuracy)+'%')
+
+
+
+
+
+
+# Get the separating hyperplane
+w = model.coef_[0]
+a = -w[0] / w[1]
+xx = np.linspace(-10000, 10000) #min - max feature value
+yy = a * xx - (model.intercept_[0]) / w[1]
+
+# Plot the parallels to the separating hyperplane that pass through the support vectors
+b = model.support_vectors_[0]
+yy_down = a * xx + (b[1] - a * b[0])
+b = model.support_vectors_[-1]
+yy_up = a * xx + (b[1] - a * b[0])
+
+# Plot the hyperplane
+sns.lmplot('Close Off High','Day Diff', data=train_data, hue='Movement', palette='Set1', fit_reg=False, scatter_kws={"s": 70})
+plt.plot(xx, yy, linewidth=2, color='black');
+plt.show()
+
+
+# Look at the margins and support vectors
+sns.lmplot('Close Off High', 'Day Diff', data=train_data, hue='Movement', palette='Set1', fit_reg=False, scatter_kws={"s": 70})
+plt.plot(xx, yy, linewidth=2, color='black')
+plt.plot(xx, yy_down, 'k--')
+plt.plot(xx, yy_up, 'k--')
+plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1],
+            s=80, facecolors='none');
+plt.show()
 
 
 
--- gatheringFeatures.py
+++ gatheringFeatures.py
@@ -9,13 +9,14 @@ bitcoin_market_info = pd.read_html(
 bitcoin_market_info = bitcoin_market_info.assign(Date=pd.to_datetime(bitcoin_market_info['Date']))
 bitcoin_market_info.loc[bitcoin_market_info['Volume'] == "-", 'Volume'] = 0
 bitcoin_market_info['Volume'] = bitcoin_market_info['Volume'].astype('int64')
-# print(bitcoin_market_info)
 
-
-# --Create Day Diff column with values--
 market_info = bitcoin_market_info
-market_info = market_info[market_info['Date'] >= '2017-01-01']
-market_info = market_info[market_info['Date'] < '2018-01-01']
+
+#--DATA only for 2017--
+# market_info = market_info[market_info['Date'] >= '2017-01-01']
+market_info = market_info[market_info['Date'] >= '2018-01-01']
+
+#--Create Day Diff column with values--
 kwargs = {'Day Diff': lambda x: (x['Open'] - x['Close']) / x['Open'] *10000}
 market_info = market_info.assign(**kwargs)
 
@@ -24,23 +25,22 @@ market_info = market_info.assign(**kwargs)
 # Close_Off_High represents the gap between the closing price and price high for that day, where values
 # of -1 and 1 mean the closing price was equal to the daily HIGH or daily LOW, respectively
 # Volatility column is simply the difference between high and low price divided by the opening price
-
 kwargs = {'Close Off High': lambda x: ((2 * (x['High'] - x['Close'])) / (x['High'] - x['Low']) -1) *10000,
           'Volatility': lambda x: (x['High'] - x['Low']) / (x['Open'])}
 market_info = market_info.assign(**kwargs)
-# print(market_info)
 
-#add Movement column
-# kwargs = {'Movement': lambda x: (x['Open'] - x['Close'])}
-# market_info = market_info.assign(**kwargs)
 
-# market_info['Movement Up/Down'] = Series(np.random.randn(len(market_info['Open'])), index=market_info.index)
+#add Movement column (initially random values)
 market_info = market_info.assign(Movement=pd.Series(np.random.randn(len(market_info['Open']))).values)
-print(market_info)
 
+#reverse array for ascending order
+market_info = market_info.sort_values(by='Date')
 
-i=120
-#Replacing Movement values wuth actual name UP or DOWN
+# Resetting the indexes
+market_info = market_info.reset_index(drop=True)
+
+#Replacing Movement random values wuth actual values -> UP or DOWN
+i=0
 while i <(len(market_info)-1):   # -1 because we cannot compare the last day with the next one
 
     market_info['Movement'][i] = market_info['Close'][i] - market_info['Close'][i+1]
@@ -51,29 +51,27 @@ while i <(len(market_info)-1):   # -1 because we cannot compare the last day wit
         market_info['Movement'][i] = 'Up'
     i=i+1
 
-# print(market_info)
+print(market_info)
 
 # drop rest keep only Close,Volume,COH & Vola
-model_data = market_info[['Date'] + [metric for metric in ['Volume', 'Close Off High', 'Day Diff', 'Volatility', 'Movement']]]
+# model_data = market_info[['Date'] + [metric for metric in ['Volume', 'Close Off High', 'Day Diff', 'Volatility', 'Movement']]]
+model_data = market_info[['Date', 'Close', 'Volume', 'Close Off High', 'Day Diff', 'Volatility', 'Movement']]
 
 
-# reverse array for ascending order
-# model_data = model_data.sort_values(by='Date')
+#Dropping the last row that doesnt have movement
+model_data = model_data.drop(model_data.index[len(model_data)-1])
+print(model_data)
 
-split_date = '2015-06-01'
-final_data = model_data[model_data['Date'] > split_date]
-# final_data = final_data.drop('Date', 1)
-final_data = final_data.drop(final_data.index[len(final_data)-1]) #dropping the last row that doesnt have movement
 
-print(final_data)
-
-with open('features2017test.csv', 'w') as csvfile:
-    fieldnames = ['Date', 'Volume', 'Close Off High', 'Day Diff', 'Volatility', 'Movement']
+with open('2018_test_data.csv', 'w') as csvfile:
+    fieldnames = ['Date', 'Close', 'Volume', 'Close Off High', 'Day Diff', 'Volatility', 'Movement']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    i=120
+
+    i=0
     while i < (len(market_info) - 1):
 
-        writer.writerow({'Date': final_data['Date'][i], 'Volume': final_data['Volume'][i],
-                     'Close Off High': final_data['Close Off High'][i], 'Day Diff': final_data['Day Diff'][i], 'Volatility': final_data['Volatility'][i], 'Movement': final_data['Movement'][i]})
+        writer.writerow({'Date': model_data['Date'][i], 'Close': model_data['Close'][i], 'Volume': model_data['Volume'][i],
+                         'Close Off High': model_data['Close Off High'][i], 'Day Diff': model_data['Day Diff'][i],
+                         'Volatility': model_data['Volatility'][i], 'Movement': model_data['Movement'][i]})
         i=i+1
